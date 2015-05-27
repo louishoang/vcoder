deb3ba8f1a419818e5fa170833b278c8c3c5853a
louis hoang <bawsing@gmail.com>
2015-05-26 19:21:48 -0700

    added user controller for front end to check user

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
                 :omniauth_callbacks => "users/omniauth_callbacks"
               }
 
-  # allow front end angularjs to get current_user
+  # allow front end angularjs to get current_user and check for user
   devise_scope:user do
     get '/api/current_user' => 'users/sessions#show_current_user'
+    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
   end
 
   root 'home#index'
